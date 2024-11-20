4fc1f33fdad4f3abea034742b2680cb1be9e1ada
Luxary <79161171+Luxary-Arxer@users.noreply.github.com>
2024-11-20 19:50:33 +0100

    Create UDP Server Whne entering in scene

--- Prop Hunt Game Online/Assets/Scripts/ServerUDP.cs
+++ Prop Hunt Game Online/Assets/Scripts/ServerUDP.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
@@ -11,18 +13,17 @@ public class ServerUDP : MonoBehaviour
 
     public GameObject UItextObj;
     TextMeshProUGUI UItext;
-    string serverText;
 
     void Start()
     {
-        UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+        //UItext = UItextObj.GetComponent<TextMeshProUGUI>();
+        startServer();
     }
 
     public void startServer()
     {
-        serverText = "Starting UDP Server...";
+        Debug.Log("Starting UDP Server...");
 
-        //TO DO 1
         //UDP doesn't keep track of our connections like TCP
         //This means that we "can only" reply to other endpoints,
         //since we don't know where or who they are
@@ -32,26 +33,25 @@ public class ServerUDP : MonoBehaviour
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         socket.Bind(ipep); // Vincula el socket al puerto 9050
 
-        serverText += "\nServer started on port 9050";
+        Debug.Log("\nServer started on port 9050");
 
-        //TO DO 3
         //Our client is sending a handshake, the server has to be able to recieve it
         //It's time to call the Receive thread
         Thread newConnection = new Thread(Receive);
         newConnection.Start();
     }
 
+    /*
     void Update()
     {
         UItext.text = serverText;
     }
-
+    */
     void Receive()
     {
         byte[] data = new byte[1024];
-        serverText += "\nWaiting for new Client...";
+        Debug.Log("\nWaiting for new Client...");
 
-        //TO DO 3
         //We don't know who may be comunicating with this server, so we have to create an
         //endpoint with any address and an IpEndpoint from it to reply to it later.
         IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
@@ -68,9 +68,8 @@ public class ServerUDP : MonoBehaviour
                int recv = socket.ReceiveFrom(data, ref Remote);
                 string message = Encoding.ASCII.GetString(data, 0, recv);
 
-                serverText += "\nMessage received from " + Remote.ToString() + ": " + message;
+                //serverText += "\nMessage received from " + Remote.ToString() + ": " + message;
 
-                //TO DO 4
                 //When our UDP server receives a message from a random remote, it has to send a ping,
                 //Call a send thread
                 Thread sendThread = new Thread(() => Send(Remote));
@@ -78,17 +77,16 @@ public class ServerUDP : MonoBehaviour
             }
             catch (SocketException e)
             {
-                serverText += "\nError receiving data: " + e.Message;
+                //serverText += "\nError receiving data: " + e.Message;
             }
         }
     }
 
     void Send(EndPoint Remote)
     {
-        //TO DO 4
         //Use socket.SendTo to send a ping using the remote we stored earlier.
         byte[] data = Encoding.ASCII.GetBytes("Ping");
         socket.SendTo(data, Remote);
-        serverText += "\nPing sent to " + Remote.ToString();
+        //serverText += "\nPing sent to " + Remote.ToString();
     }
 }
