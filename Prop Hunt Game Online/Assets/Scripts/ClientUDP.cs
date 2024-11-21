c91b1b4038544e3af4ab883103dbcd4eb1c9b886
Guillem <guillemaixut@gmail.com>
2024-11-21 02:29:49 +0100

    No Fallos
    En teoria no falla nada, pero tpk funcoina lo del cubo

--- Prop Hunt Game Online/Assets/Scripts/ClientUDP.cs
+++ Prop Hunt Game Online/Assets/Scripts/ClientUDP.cs
@@ -8,58 +8,46 @@ using TMPro;
 public class ClientUDP : MonoBehaviour
 {
     Socket socket;
-
-    string clientText;
     public string IPServer;
-    public string NamePlayer;
-
-    public void Awake()
-    {
-        IPServer = JoinInformation.client_Home.clientIP;
-
-        Debug.Log("Server IP: " + IPServer);
-
-        NamePlayer = JoinInformation.client_Home.clientName;
-        read_Name(NamePlayer);
-
-        Debug.Log("Player name: " + NamePlayer);
-    }
+    private bool connected = false;
 
     void Start()
     {
-        Send();
+        IPServer = JoinInformation.client_Home.clientIP;
+        StartClient();
     }
 
     public void StartClient()
     {
-        Thread mainThread = new Thread(Send);
-        mainThread.Start();
-    }
-
-    void Update()
-    {
-        Send();
+        Thread sendThread = new Thread(Send);
+        sendThread.Start();
     }
 
     void Send()
     {
-        // Send position data every frame
         IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(IPServer), 9050);
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-        // Simulating player position (can be replaced with actual player position)
-        Vector3 playerPosition = transform.position;
+        while (true)
+        {
+            // Get the player position
+            Vector3 playerPosition = transform.position;
 
-        string message = "Position: " + playerPosition.x + "," + playerPosition.y + "," + playerPosition.z;
-        byte[] data = Encoding.ASCII.GetBytes(message);
-        socket.SendTo(data, ipep);
+            // Send position data
+            string message = "Position: " + playerPosition.x + "," + playerPosition.y + "," + playerPosition.z;
+            byte[] data = Encoding.ASCII.GetBytes(message);
+            socket.SendTo(data, ipep);
+            Debug.Log("Position sent: " + message);
 
-        clientText = "Position sent to " + ipep.ToString() + ": " + message;
-        Debug.Log(clientText);
+            // Try receiving the server response only once
+            if (!connected)
+            {
+                Receive();
+                connected = true;
+            }
 
-        // Receive server response
-        Thread receive = new Thread(Receive);
-        receive.Start();
+            Thread.Sleep(500); // Send data every 500ms
+        }
     }
 
     void Receive()
@@ -70,32 +58,13 @@ public class ClientUDP : MonoBehaviour
 
         try
         {
-            while (true)
-            {
-                int recv = socket.ReceiveFrom(data, ref remote);
-                string receivedMessage = Encoding.ASCII.GetString(data, 0, recv);
-                clientText += "\nMessage received from " + remote.ToString() + ": " + receivedMessage;
-
-                // Log the server's response
-                Debug.Log("Message received from server: " + receivedMessage);
-            }
+            int recv = socket.ReceiveFrom(data, ref remote);
+            string receivedMessage = Encoding.ASCII.GetString(data, 0, recv);
+            Debug.Log("Message received from server: " + receivedMessage);
         }
         catch (SocketException e)
         {
-            clientText += "\nError receiving data: " + e.Message;
+            Debug.LogError("Error receiving data: " + e.Message);
         }
-    }
-
-    public void read_IP(string IP)
-    {
-        IPServer = IP;
-        Debug.Log("Server IP set to: " + IPServer);
-    }
-
-    public void read_Name(string Name)
-    {
-        NamePlayer = Name;
-        DisplayPlayerName.NamePlayer_1 = NamePlayer;
-        Debug.Log("Player name updated to: " + NamePlayer);
     }
 }
--- Prop Hunt Game Online/Assets/Scripts/ServerUDP.cs
+++ Prop Hunt Game Online/Assets/Scripts/ServerUDP.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
@@ -12,11 +14,13 @@ public class ServerUDP : MonoBehaviour
     public GameObject UItextObj;
     TextMeshProUGUI UItext;
 
-    public string NamePlayer = "No Name";
+    // Cube to move based on player position
+    public GameObject playerCube;
 
     private void Awake()
     {
-        NamePlayer = ChangeScene.server_Home.serverName;
+        // Initialize the player name
+        string NamePlayer = "Server"; // Can be customized
         read_Name(NamePlayer);
         Debug.Log(NamePlayer);
     }
@@ -30,13 +34,14 @@ public class ServerUDP : MonoBehaviour
     {
         Debug.Log("Starting UDP Server...");
 
+        // UDP binding to receive data on port 9050
         IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
         socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         socket.Bind(ipep);
 
-        Debug.Log("Server started on port 9050");
+        Debug.Log("\nServer started on port 9050");
 
-        // Create thread to receive messages
+        // Thread to receive data
         Thread newConnection = new Thread(Receive);
         newConnection.Start();
     }
@@ -44,29 +49,42 @@ public class ServerUDP : MonoBehaviour
     void Receive()
     {
         byte[] data = new byte[1024];
-        Debug.Log("Waiting for new Client...");
+        Debug.Log("\nWaiting for new Client...");
 
         IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-        EndPoint remote = (EndPoint)sender;
+        EndPoint Remote = (EndPoint)(sender);
 
+        // Loop to continuously receive data
         while (true)
         {
             try
             {
-                int recv = socket.ReceiveFrom(data, ref remote);
+                int recv = socket.ReceiveFrom(data, ref Remote);
                 string message = Encoding.ASCII.GetString(data, 0, recv);
 
-                // Process message from client (e.g., player name or position data)
-                Debug.Log("Message received from " + remote.ToString() + ": " + message);
+                // Log when a message is received from a client
+                Debug.Log("Received message from " + Remote.ToString() + ": " + message);
 
-                // If the message is position data, log the received position
-                if (message.Contains("Position"))
+                // Check for position update message
+                if (message.Contains("Position:"))
                 {
-                    Debug.Log("Position received: " + message);
+                    // Parse the position
+                    string[] positionData = message.Split(':')[1].Trim().Split(',');
+
+                    if (positionData.Length == 3)
+                    {
+                        // Parse position values from the message
+                        float x = float.Parse(positionData[0]);
+                        float y = float.Parse(positionData[1]);
+                        float z = float.Parse(positionData[2]);
+
+                        // Update the cube's position
+                        UpdateCubePosition(new Vector3(x, y, z));
+                    }
                 }
 
-                // Handle response by pinging back the client
-                Thread sendThread = new Thread(() => Send(remote));
+                // Start a new thread to send a ping back to the client (or acknowledge)
+                Thread sendThread = new Thread(() => Send(Remote));
                 sendThread.Start();
             }
             catch (SocketException e)
@@ -76,17 +94,29 @@ public class ServerUDP : MonoBehaviour
         }
     }
 
-    void Send(EndPoint remote)
+    // Method to update the cube's position based on the received data
+    void UpdateCubePosition(Vector3 newPosition)
+    {
+        if (playerCube != null)
+        {
+            playerCube.transform.position = newPosition;
+            Debug.Log("Cube position updated: " + newPosition);
+        }
+        else
+        {
+            Debug.LogError("Player Cube not assigned in the server!");
+        }
+    }
+
+    void Send(EndPoint Remote)
     {
         byte[] data = Encoding.ASCII.GetBytes("Ping");
-        socket.SendTo(data, remote);
-        Debug.Log("Ping sent to " + remote.ToString());
+        socket.SendTo(data, Remote);
+        Debug.Log("Ping sent to " + Remote.ToString());
     }
 
     public void read_Name(string Name)
     {
-        NamePlayer = Name;
-        DisplayPlayerName.NamePlayer_1 = NamePlayer;
-        Debug.Log("Player name set to: " + NamePlayer);
+        Debug.Log("Server name set to: " + Name);
     }
 }
