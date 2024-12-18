4ea48a0e66d9ea5cbd0a88d514e232ca934a52e3
Luxary <79161171+Luxary-Arxer@users.noreply.github.com>
2024-12-18 21:04:33 +0100

    Player Canges

--- Prop Hunt Game Online/Assets/Scripts/Gameplay/PlayerToProp.cs
+++ Prop Hunt Game Online/Assets/Scripts/Gameplay/PlayerToProp.cs
@@ -54,16 +54,14 @@ public class PlayerTransformation : MonoBehaviour
         // Destruir el modelo actual
         if (currentModel != null)
         {
-
             CaraterMesh.SetActive(false);
             Destroy(currentModel);
             Debug.Log("Modelo actual destruido.");
         }
 
-
-
         // Crear un nuevo modelo basado en el objetivo
         GameObject newModel = Instantiate(targetObject, modelParent);
+        newModel.layer = 7;
         newModel.transform.localPosition = new Vector3(0f,0.6f,-0.5f);
         newModel.transform.localRotation = Quaternion.identity;
 
