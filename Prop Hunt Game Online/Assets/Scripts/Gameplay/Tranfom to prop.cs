50b6112876fd026d0b77c05a9200bfdfdec77297
Guillem <guillemaixut@gmail.com>
2024-12-18 17:17:55 +0100

    TransforToProp
    Funciona, pero me quedo clabado en el suelo (no me muevo)

--- Prop Hunt Game Online/Assets/Scripts/Gameplay/Tranfom to prop.cs
+++ Prop Hunt Game Online/Assets/Scripts/Gameplay/Tranfom to prop.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Tranfomtoprop : MonoBehaviour
+public class ObjectTransformation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public LayerMask transformLayer; // Capa para los objetos transformables
+    public float maxDistance = 5f; // Distancia máxima para transformar
+    private Camera playerCamera;
+
     void Start()
     {
-        
+        playerCamera = Camera.main; // Obtiene la cámara principal
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TryTransform();
+        }
+    }
+
+    void TryTransform()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2)); // Ray desde el centro de la pantalla
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxDistance, transformLayer))
+        {
+            GameObject targetObject = hit.collider.gameObject;
+
+            // Verifica que tenga un MeshFilter y un MeshRenderer
+            MeshFilter targetMeshFilter = targetObject.GetComponent<MeshFilter>();
+            MeshRenderer targetMeshRenderer = targetObject.GetComponent<MeshRenderer>();
+
+            if (targetMeshFilter != null && targetMeshRenderer != null)
+            {
+                MeshFilter playerMeshFilter = GetComponent<MeshFilter>();
+                MeshRenderer playerMeshRenderer = GetComponent<MeshRenderer>();
+
+                // Cambia el Mesh y el Material del jugador
+                playerMeshFilter.mesh = targetMeshFilter.mesh;
+                playerMeshRenderer.materials = targetMeshRenderer.materials;
+
+                Debug.Log("Transformed into: " + targetObject.name);
+            }
+        }
     }
 }
