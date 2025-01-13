55a3947d18e22d6c174492fab06c83ac24415f3f
Luxary <79161171+Luxary-Arxer@users.noreply.github.com>
2025-01-13 23:47:35 +0100

    constador partida

--- Prop Hunt Game Online/Assets/Entrega final/Doors_Coutdown.cs
+++ Prop Hunt Game Online/Assets/Entrega final/Doors_Coutdown.cs
@@ -13,13 +13,26 @@ public class Doors_Coutdown : MonoBehaviour
     public TextMeshPro Contador1;
     public TextMeshPro Contador2;
     public TextMeshPro Contador3;
+    public TextMeshPro ContadorGrande;
+    public Transform Player;
+    public GameObject Consola_1;
+    public GameObject Consola_2;
+    public float TiempoPartida = 2;
 
+    private void Start()
+    {
+        Contador1.text = "Press E on the console to start the coutdown";
+        Contador2.text = "Press E on the console to start the coutdown";
+        Contador3.text = "Press E on the console to start the coutdown";
+        ContadorGrande.text = "Press E on the console to start the coutdown";
+    }
     void Update()
     {
         float interact_Range = 2f;
         Collider[] collider_array = Physics.OverlapSphere(transform.position, interact_Range);
 
         //StartCoroutine("Countdown", StartCoutdown);
+        ContadorGrande.transform.LookAt(new Vector3(Player.position.x, Player.position.y, Player.position.z));
 
         foreach (Collider collider in collider_array)
         {
@@ -35,12 +48,15 @@ public class Doors_Coutdown : MonoBehaviour
     }
     IEnumerator Countdown(int seconds)
     {
+        Consola_1.transform.position = new Vector3(0, -8, 0);
+        Consola_2.transform.position = new Vector3(0, -8, 0);
         int counter = seconds;
         while (counter > 0)
         {
-            Contador1.text = "The match will\nstart in:" + counter.ToString();
-            Contador2.text = "The match will\nstart in:"+counter.ToString();
-            Contador3.text = "The match will\nstart in:"+counter.ToString();
+            Contador1.text = "The match will\nstart in: " + counter.ToString();
+            Contador2.text = "The match will\nstart in: "+counter.ToString();
+            Contador3.text = "The match will\nstart in: "+counter.ToString();
+            ContadorGrande.text = "The match will\nstart in: " + counter.ToString();
             yield return new WaitForSeconds(1);
             counter--;
         }
@@ -49,5 +65,21 @@ public class Doors_Coutdown : MonoBehaviour
         Contador1.text = "GO";
         Contador2.text = "GO";
         Contador3.text = "GO";
+        ContadorGrande.text = "GO";
+        StartCoroutine("TiempoRestante", TiempoPartida);
+    }
+    IEnumerator TiempoRestante(int seconds)
+    {
+        int counter = seconds;
+        while (counter > 0)
+        {
+            Contador1.text = "Tiempo partida: " + counter.ToString();
+            Contador2.text = "Tiempo partida: " + counter.ToString();
+            Contador3.text = "Tiempo partida: " + counter.ToString();
+            ContadorGrande.text = "Tiempo partida: " + counter.ToString();
+            yield return new WaitForSeconds(1);
+            counter--;
+        }
+        Player.transform.position = new Vector3(0, 1, 0);
     }
 }
